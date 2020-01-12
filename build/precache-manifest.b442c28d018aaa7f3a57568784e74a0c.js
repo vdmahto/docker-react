6b60e30b30f9ea1f17b63d747cf07daf67cb674e
Vishnu D Mahto <vismaht1@in.ibm.com>
2020-01-12 18:52:45 +0530

    first commit

---
+++ build/precache-manifest.b442c28d018aaa7f3a57568784e74a0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72bb88ca3a743faf955e8fe2c94971bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a51f128cb5a903ea0758",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e1479426efe1a789fd90",
+    "url": "/static/js/2.db6de22c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.db6de22c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a51f128cb5a903ea0758",
+    "url": "/static/js/main.8dd4cb5b.chunk.js"
+  },
+  {
+    "revision": "cd026b5230fa457ca100",
+    "url": "/static/js/runtime-main.37397318.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
